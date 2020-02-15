e2fc60dbcdddc2c4bd4bc437dfe23d469599cfe3
Gabriella A. Martinez Viloria <52822208+martinezga@users.noreply.github.com>
2020-02-15 12:42:05 -0300

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,11 @@
 const addFigToTrends = data => {
-    const container = document.querySelector('#add-images');
+    const divContainer = document.querySelector('#add-images');
     const figures = document.createElement('figure');
     const images = document.createElement('img');
-    container.appendChild(figures);
+    divContainer.appendChild(figures);
     figures.appendChild(images);
     images.setAttribute("src", "images/2-hours-finding-error.jpg");
-    console.log(container.outerHTML)
 }
-//addFigToTrends()
+addFigToTrends()
+addFigToTrends()
+addFigToTrends()
