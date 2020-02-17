ca285f725d92bfa54b61f28f170619e2baecccef
Gabriella A. Martinez Viloria <52822208+martinezga@users.noreply.github.com>
2020-02-17 06:02:33 -0300

    trends js

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-const addFigToTrends = data => {
+const addFigToT = data => {
     const divContainer = document.querySelector('#add-images');
     const figures = document.createElement('figure');
     const images = document.createElement('img');
@@ -6,6 +6,23 @@ const addFigToTrends = data => {
     figures.appendChild(images);
     images.setAttribute("src", "images/2-hours-finding-error.jpg");
 }
-addFigToTrends()
-addFigToTrends()
-addFigToTrends()
+async function searchTrends() {
+    const url = 'https://api.giphy.com/v1/gifs/trending?api_key=kdFOwDT4ieXpQiNeUk4B1EhjZ0yt0Irt&limit=25&rating=G';
+    const answer = await fetch(url);
+    const data = await answer.json();
+    console.log(data.data[0].bitly_gif_url)
+    return data
+}
+const addFigToTrends = data => {
+    const trends = searchTrends();
+    trends.then(function(resp) {
+        console.log(resp)
+        const divContainer = document.querySelector('#add-images');
+        const figures = document.createElement('figure');
+        const images = document.createElement('img');
+        divContainer.appendChild(figures);
+        figures.appendChild(images);
+        images.setAttribute("src", resp.data[0].bitly_gif_url);
+    });
+}
+addFigToTrends() 
